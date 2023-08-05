7b406b295d559f810fd3fca4c486f178e27159c5
unknown <simplecarrier101@gmail.com>
2023-08-04 22:04:28 -0700

    added some navbar menu

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,51 @@
+
+// Navbar.js
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <Link to="/" className="text-white text-2xl font-semibold">
+            My Portfolio
+          </Link>
+          <div className="hidden md:flex space-x-4">
+            <NavLink
+              to="/"
+              end
+              activeClassName="text-white"
+              className="text-gray-300 hover:text-white transition duration-300"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/skills"
+              activeClassName="text-white"
+              className="text-gray-300 hover:text-white transition duration-300"
+            >
+              Skills
+            </NavLink>
+            <NavLink
+              to="/projects"
+              activeClassName="text-white"
+              className="text-gray-300 hover:text-white transition duration-300"
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="/contact"
+              activeClassName="text-white"
+              className="text-gray-300 hover:text-white transition duration-300"
+            >
+              Contact
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// main.jsx or App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import Home from   "./Components/Home"
+import Skills from  "./Components/Skills"
+import Projects from "./Components/Projects"
+import Contact from  "./Components/Contact"
+
+const Main = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Main;
