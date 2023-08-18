368652a672e14e403fab211ef69227d2c0de5dec
unknown <simplecarrier101@gmail.com>
2023-08-17 23:05:22 -0700

    css file for the navbar

--- src/Components/NavBar/NavBarBS.jsx
+++ src/Components/NavBar/NavBarBS.jsx
@@ -1,38 +1,54 @@
 
-
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import React,{useState} from 'react';
-import Home from '../Home';  
-import Portfolio  from '../Portfolio'; 
-import Projects from '../Projects';  
-import Contact from '../Contact'; 
-
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './NavBarBS.css'; // Import your custom CSS
 
 const NavBarBS = () => {
+  const [expanded, setExpanded] = useState(false); // State for expanded navbar
 
- 
-  
-
+  // Toggle the expanded state when the navbar toggler is clicked
+  const handleNavToggle = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar
+        bg="dark"
+        data-bs-theme="dark"
+        expand="lg"
+        expanded={expanded} // Pass the expanded state to the Navbar component
+      >
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>  {/* Corrected href value */}
-
-            
-          </Nav>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={handleNavToggle} // Call the toggle function
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/portfolio">
+                Portfolio
+              </Nav.Link>
+              <Nav.Link as={Link} to="/projects">
+                Projects
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      
     </>
   );
-}
+};
 
 export default NavBarBS;
+
+
+
