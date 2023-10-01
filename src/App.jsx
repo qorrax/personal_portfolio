431ca66fbf51a8d45caff8cf87a1cffffc7e826e
unknown <simplecarrier101@gmail.com>
2023-09-30 20:17:28 -0700

    added some code for the about file

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,27 @@
 
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import NavBarBS from './Components/NavBar/NavBarBS';
-// import Home from './Components/Home';
-// import  Portfolio from './Components/Portfolio';
-// import Projects from './Components/Projects';
-// import Contact from './Components/Contact';
-
-// const App = () => {
-//   return (
-//     <div>
-//       <NavBarBS />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/Porfolio" element={<Portfolio />} />
-//         <Route path="/Projects" element={<Projects />} />
-//         <Route path="/Contact" element={<Contact />} />
-
-//         {/* Add a catch-all route */}
-//         <Route path="*" element={<Home />} />
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default App;
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBarBS from './Components/NavBar/NavBarBS';
-import Footer from './Components/Footer'; // Import the Footer component
-import Home from './Components/About';
-// import Projects  from './Components/Projects';
+import  About from './Components/About';
 import Skills from './Components/Skills';
 import Contact from './Components/Contact';
-import Projects from './Components/Projects'; // Correct the import case
+import Projects from './Components/Projects'; 
+import Footer from './Components/Footer'; 
 
 const App = () => {
   return (
     <div>
       <NavBarBS />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
         <Route path="/projects" element={<Projects />} /> {/* Correct the route path case */}
         <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
         
         {/* Add a catch-all route */}
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<About />} />
       </Routes>
       <Footer /> {/* Include the Footer component */}
     </div>
@@ -57,3 +29,7 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,99 +1,62 @@
 
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
-import Footer from './Footer';
+
+import React, { useState } from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 import Skills from './Skills';
 import Projects from './Projects';
+import Contact from './Contact';
 
+const About = () => {
+  const [showProjects, setShowProjects] = useState(false);
 
+  const toggleProjects = () => {
+    setShowProjects(!showProjects);
+  };
 
-
-   
-
-
-
-
-  const About = () => {
-    return (
-      <div name='home' className='w-full h-screen bg-[#0a192f]'>
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <div className='flex-grow bg-[#0a192f]'>
         {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-          <p className='text-pink-600 text-3xl' >Hi, my name is</p>
-          <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>
-            Abdulkadir Mohamed 
+        <div className='max-w-[1000px] mx-auto px-8 py-4 flex flex-col justify-center h-full'>
+          <p className='text-pink-600 text-3xl'>Hi, my name is</p>
+          <h1 className='text-4xl pt-2 sm:text-6xl font-bold text-[#ccd6f6]'>
+            Abdulkadir Mohamed
           </h1>
-          
-          <p className='text-[#8892b0] py-4 max-w-[700px]'>
-            I’m a full-stack developer specializing in building (and occasionally
-            designing) exceptional digital experiences. Currently, I’m focused on
-            building responsive full-stack web applications.
+
+          <p className='text-[#8892b0] py-2 max-w-[700px]'>
+            I’m a full-stack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building responsive full-stack web applications.
           </p>
           <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            <button
+              onClick={toggleProjects}
+              className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
+            >
               View Work
               <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-3 ' />
               </span>
             </button>
           </div>
-          <Skills />
-          <Projects />
-          
-          
         </div>
-        <Footer />
       </div>
-    );
-  };
-  
-  export default About;
 
+      {/* <div className='bg-[#0a192f]'>
+        {showProjects && <Projects />}
+        <Skills />
+        <Contact />
+      </div> */}
+      <div className='bg-[#0a192f]'>
+  {showProjects && <Projects />}
+  <div className='p-4 md:p-8 lg:p-12'>
+    <Skills />
+    <div className='my-4 lg:my-8'></div> {/* Add margin for separation */}
+    <Contact />
+  </div>
+</div>
 
-// import React, { useState } from 'react';
-// import { HiArrowNarrowRight } from 'react-icons/hi';
-// import Projects from './Projects'; // Import your Projects component
+    </div>
+  );
+};
 
-// const About = () => {
-//   const [showProjects, setShowProjects] = useState(false);
-
-//   const toggleProjects = () => {
-//     setShowProjects(!showProjects);
-//   };
-
-//   return (
-//     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-//       {/* ... */}
-//       <div>
-//         <div className={`${showProjects ? 'hidden' : 'block'}`}> {/* Hide content when Projects is shown */}
-//           {/* Your existing content */}
-//           <p className='text-pink-600 text-3xl'>Hi, my name is</p>
-//           <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>
-//             Abdulkadir Mohamed 
-//           </h1>
-          
-//           <p className='text-[#8892b0] py-4 max-w-[700px]'>
-//             I’m a full-stack developer specializing in building (and occasionally
-//             designing) exceptional digital experiences. Currently, I’m focused on
-//             building responsive full-stack web applications.
-//           </p>
-//         </div>
-
-//         <button
-//           className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
-//           onClick={toggleProjects}
-//         >
-//           View Work
-//           <span className={`group-hover:rotate-90 duration-300 ${showProjects ? 'rotate-90' : ''}`}>
-//             <HiArrowNarrowRight className='ml-3 ' />
-//           </span>
-//         </button>
-        
-//         {showProjects && <Projects />} {/* Conditionally render Projects */}
-//       </div>
-//       {/* ... */}
-//     </div>
-//   );
-// };
-
-// export default About;
+export default About;
 
